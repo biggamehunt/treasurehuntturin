a4969ec7cbe52e0184d9a4483d3553559def4de7
Biggamehunt <treasurehuntturin@gmail.com>
2016-06-23 12:36:08 +0200

    asdwqea

--- app/src/main/java/com/example/andrea22/gamehunt/NewHuntActivity.java
+++ app/src/main/java/com/example/andrea22/gamehunt/NewHuntActivity.java
@@ -1,23 +1,11 @@
 package com.example.andrea22.gamehunt;
 
-import android.app.Activity;
+
 import android.app.DialogFragment;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.example.andrea22.gamehunt.utility.DBHelper;
-
+import layout.DatePickerFragment;
 import layout.TimePickerFragment;
 
 /**
@@ -26,17 +14,22 @@ import layout.TimePickerFragment;
 public class NewHuntActivity extends FragmentActivity {
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_newhunt);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newhunt);
+    }
 
 
 
     public void showTimePickerDialog(View v) {
-      DialogFragment newFragment = new TimePickerFragment();
-         newFragment.show(getFragmentManager(), "timePicker2222");
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 
 
