140d1ffe438c3a3fb76007fc2a5a44b403f03e10
Biggamehunt <treasurehuntturin@gmail.com>
2016-06-24 22:50:29 +0200

    popopopo

--- app/src/main/java/com/example/andrea22/gamehunt/HuntListActivity.java
+++ app/src/main/java/com/example/andrea22/gamehunt/HuntListActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,48 +20,57 @@ import com.example.andrea22.gamehunt.utility.DBHelper;
  */
 public class HuntListActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_huntlist);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huntlist);
 
-            LinearLayout huntsContainer = (LinearLayout) findViewById(R.id.huntsContainer);
+        LinearLayout huntsContainer = (LinearLayout) findViewById(R.id.huntsContainer);
 
-            DBHelper mDbHelper = DBHelper.getInstance(getApplicationContext());
-            SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-
-
-            Cursor c = db.rawQuery("SELECT * FROM HUNT ", null);
-            if(c.moveToFirst()){
-                do{
-                    LayoutInflater factory = LayoutInflater.from(this);
-                    LinearLayout myView = (LinearLayout) factory.inflate(R.layout.hunt, null);
-                    LinearLayout child = (LinearLayout) myView.getChildAt(0);
-                    TextView name = (TextView) child.getChildAt(0);
-                    TextView timeStart = (TextView) child.getChildAt(1);
-                    Log.v("db log", "name db: " + c.getString(0)
-                    );
-                    name.setText(c.getString(c.getColumnIndex("name")));
-                    timeStart.setText(c.getString(c.getColumnIndex("timeStart")));
-                    myView.setId(Integer.parseInt(c.getString(c.getColumnIndex("idHunt"))));
-                    //toDo: ProgressBar e Matita
-
-                    huntsContainer.addView(myView);
-                    Log.v("db log", "aggiunta view");
-
-                }while(c.moveToNext());
-            }
+        DBHelper mDbHelper = DBHelper.getInstance(getApplicationContext());
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
 
+        Cursor c = db.rawQuery("SELECT * FROM HUNT ", null);
+        if (c.moveToFirst()) {
+            do {
+                LayoutInflater factory = LayoutInflater.from(this);
+                LinearLayout myView = (LinearLayout) factory.inflate(R.layout.hunt, null);
+                LinearLayout child = (LinearLayout) myView.getChildAt(0);
+                TextView name = (TextView) child.getChildAt(0);
+                TextView timeStart = (TextView) child.getChildAt(1);
+                Log.v("db log", "name db: " + c.getString(0)
+                );
+                name.setText(c.getString(c.getColumnIndex("name")));
+                timeStart.setText(c.getString(c.getColumnIndex("timeStart")));
+                myView.setId(Integer.parseInt(c.getString(c.getColumnIndex("idHunt"))));
+                //toDo: ProgressBar e Matita
+
+                huntsContainer.addView(myView);
+                Log.v("db log", "aggiunta view");
+
+            } while (c.moveToNext());
         }
+/**
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+**/
 
-    public void goToHunt(View view)  {
+    }
 
-        Log.v("db log", "id: " +view.getId());
+    public void goToHunt(View view) {
+
+        Log.v("db log", "id: " + view.getId());
         Intent intent = new Intent(this, NewHuntActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.enter, R.anim.exit);
 
     }
 }
+
--- app/src/main/java/com/example/andrea22/gamehunt/LoginActivity.java
+++ app/src/main/java/com/example/andrea22/gamehunt/LoginActivity.java
@@ -2,18 +2,25 @@ package com.example.andrea22.gamehunt;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.andrea22.gamehunt.utility.DBHelper;
 import com.example.andrea22.gamehunt.utility.RetrieveFeedTask;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,20 +34,27 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 
 
-
-
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
-     protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void login(View view)  {
+    public void login(View view) {
 
 
         EditText usernameview = (EditText) findViewById(R.id.username);
@@ -57,7 +71,7 @@ public class LoginActivity extends AppCompatActivity {
                 String u = "http://jbossews-treasurehunto.rhcloud.com/ProfileOperation?action=login&username=" + username + "&password=" + password;
                 String res = new RetrieveFeedTask().execute(u).get();
 
-                if (!res.equals("0")){
+                if (!res.equals("0")) {
                     DBHelper myHelper = DBHelper.getInstance(getApplicationContext());
                     SQLiteDatabase db = myHelper.getWritableDatabase();
                     myHelper.createDB(db, res);
@@ -67,31 +81,78 @@ public class LoginActivity extends AppCompatActivity {
                     CharSequence text = getString(R.string.login_error);
                     int duration = Toast.LENGTH_SHORT;
 
-                       Toast toast = Toast.makeText(this, text, duration);
-                       toast.show();
+                    Toast toast = Toast.makeText(this, text, duration);
+                    toast.show();
                 }
 
 
-
             } catch (Exception e) {
-                Log.d("test debug", "eccezione: "+e.getMessage());
+                Log.d("test debug", "eccezione: " + e.getMessage());
                 e.printStackTrace();
             }
 
 
         } catch (Exception e) {
-            Log.d("test debug", "eccezione: "+e.getMessage());
+            Log.d("test debug", "eccezione: " + e.getMessage());
             e.printStackTrace();
 
         }
 
     }
+//TODO: inserire questa funzione in tutte le activity che usano tastiera
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+    }
 
-    public void registration(View view)  {
+
+
+    public void registration(View view) {
         Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Login Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.andrea22.gamehunt/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Login Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.andrea22.gamehunt/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
 
 
