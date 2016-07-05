66d4a0bd963ae4947e37e4018cbe3c0add771ff5
Hometogether <hometogether92@gmail.com>
2016-07-05 16:10:03 +0200

    database revolution

--- app/src/main/java/com/example/andrea22/gamehunt/Database/BeTable.java
+++ app/src/main/java/com/example/andrea22/gamehunt/Database/BeTable.java
@@ -11,6 +11,7 @@ public class BeTable {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+
                         COLUMN_IDBE + " INTEGER PRIMARY KEY NOT NULL, " +
                         COLUMN_IDUSER + " INTEGER NOT NULL, " +
                         COLUMN_IDTEAM + " INTEGER NOT NULL, " +
