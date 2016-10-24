5f85d89db1d893ec8174098ca7b99cba18be5fc4
Biggamehunt <treasurehuntturin@gmail.com>
2016-10-24 16:45:12 +0200

    popo

--- app/src/main/java/com/example/andrea22/gamehunt/GalleryActivity.java
+++ app/src/main/java/com/example/andrea22/gamehunt/GalleryActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.andrea22.gamehunt.Database.DBHelper;
@@ -23,8 +22,6 @@ import java.util.ArrayList;
 
 public class GalleryActivity extends AppCompatActivity {
 
-    private String TAG = GalleryActivity.class.getSimpleName();
-    private static final String endpoint = "https://drive.google.com/open?id=0B9gNK0lCV_TgRk9uTl9YdUNRQ3M";
     private ArrayList<Image> images;
     private ProgressDialog pDialog;
     public GalleryAdapter mAdapter;
@@ -34,10 +31,6 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.gallery_toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle("");
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
@@ -49,17 +42,12 @@ public class GalleryActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
-//ciAOOoooo
 
         DBHelper mDbHelper = DBHelper.getInstance(getApplicationContext());
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
         SharedPreferences pref = getSharedPreferences("session", MODE_PRIVATE);
 
-
-
         ArrayList<InfoHuntForCheck> hunts = mDbHelper.getHuntsFromUser(db, pref.getInt("idUser", 0));
-
-
 
         recyclerView.addOnItemTouchListener(new GalleryAdapter.RecyclerTouchListener(getApplicationContext(), recyclerView, new GalleryAdapter.ClickListener() {
             @Override
@@ -80,67 +68,6 @@ public class GalleryActivity extends AppCompatActivity {
             }
         }));
 
-
         new GetPhoto(this).execute(hunts);
-
-
     }
-
-    /*private void fetchImages() {
-
-        pDialog.setMessage("Downloading images...");
-        pDialog.show();
-
-
-
-        JsonArrayRequest req = new JsonArrayRequest(endpoint,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.d(TAG, response.toString());
-                        pDialog.hide();
-
-                        images.clear();
-
-                        for (int i = 0; i < response.length(); i++) {
-
-                            Log.d("nel for","lunghezza response = " +response.length());
-
-                            try {
-
-                                Log.d("nel try","");
-
-                                JSONObject object = response.getJSONObject(i);
-                                Image image = new Image();
-                                image.setName(object.getString("name"));
-
-                                JSONObject url = object.getJSONObject("url");
-                                image.setSmall(url.getString("small"));
-                                image.setMedium(url.getString("medium"));
-                                image.setLarge(url.getString("large"));
-                                image.setTimestamp(object.getString("timestamp"));
-
-                                Log.d("nel try","url = " +url);
-
-
-                                images.add(image);
-
-                            } catch (JSONException e) {
-                                Log.e(TAG, "Json parsing error: " + e.getMessage());
-                            }
-                        }
-
-                        mAdapter.notifyDataSetChanged();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "Error: " + error.getMessage());
-                pDialog.hide();
-            }
-        });
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(req);
-    }*/
 }
